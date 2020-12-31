efde455f6d11b331bf177b01e3eb37c515ffe9b3
Karl Jose Buena <karljosebuena@gmail.com>
2020-12-31 16:00:31 +0800

    integration-testing-demo: VoterComponent - updated test for component using a service

--- testing-demo/src/app/integration-testing-demo/2-todos/todos.component.spec.ts
+++ testing-demo/src/app/integration-testing-demo/2-todos/todos.component.spec.ts
@@ -1,35 +1,41 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TodosComponent } from './todos.component';
+import { TodoService } from './todo.service';
+import { HttpModule } from '@angular/http';
+import { from } from 'rxjs';
 
-//NOTE: I've deliberately excluded this suite from running
-// because the test will fail. This is because we have not 
-// provided the TodoService as a dependency to TodosComponent. 
-// 
-// When you get to Lecture 6 (Providing Dependencies), be sure
-// to remove "x" from "xdescribe" below. 
-
-xdescribe('TodosComponent', () => {
+describe('TodosComponent', () => {
   let component: TodosComponent;
   let fixture: ComponentFixture<TodosComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ TodosComponent ]
+      imports: [HttpModule],
+      declarations: [TodosComponent],
+      providers: [TodoService]
     })
-    .compileComponents();
-  }));
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(TodosComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should load todos from the server', () => {
+    const response = [1, 2, 3];
+    const service = TestBed.get(TodoService);
+    spyOn(service, 'getTodos').and.returnValue(
+      from([response])
+    );
+
+    fixture.detectChanges();
+
+    expect(component.todos).toBe(response);
   });
 });
--- testing-demo/src/app/integration-testing-demo/2-todos/todos.component.ts
+++ testing-demo/src/app/integration-testing-demo/2-todos/todos.component.ts
@@ -8,15 +8,15 @@ import { TodoService } from './todo.service'
 })
 export class TodosComponent implements OnInit {
   todos: any[] = [];
-  message; 
+  message;
 
   constructor(private service: TodoService) {}
 
-  ngOnInit() { 
+  ngOnInit() {
     this.service.getTodos().subscribe(t => this.todos = t);
   }
 
-  add() { 
+  add() {
     var newTodo = { title: '... ' };
     this.service.add(newTodo).subscribe(
       t => this.todos.push(t),
@@ -26,5 +26,5 @@ export class TodosComponent implements OnInit {
   delete(id) {
     if (confirm('Are you sure?'))
       this.service.delete(id).subscribe();
-  } 
+  }
 }
