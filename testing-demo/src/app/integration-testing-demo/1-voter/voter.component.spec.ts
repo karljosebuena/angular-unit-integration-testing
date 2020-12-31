714b0ffd448b33b993e7768ca711ca2c63f5c8a9
Karl Jose Buena <karljosebuena@gmail.com>
2020-12-31 14:01:02 +0800

    integration-testing-demo: VoterComponent - basic setup for unit testing.

--- testing-demo/src/app/integration-testing-demo/1-voter/voter.component.spec.ts
+++ testing-demo/src/app/integration-testing-demo/1-voter/voter.component.spec.ts
@@ -1,8 +1,20 @@
+import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { VoterComponent } from './voter.component';
 
 describe('VoterComponent', () => {
 
   beforeEach(() => {
+    let component: VoterComponent;
+    let fixture: ComponentFixture<VoterComponent>;
+
+    TestBed.configureTestingModule({
+      declarations: [
+        VoterComponent
+      ]
+    });
+
+    fixture = TestBed.createComponent(VoterComponent);
+    component = fixture.componentInstance;
   });
 
   it('', () => {
