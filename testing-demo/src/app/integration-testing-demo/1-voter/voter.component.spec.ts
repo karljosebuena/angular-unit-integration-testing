c8b65f1aa5750bb2b481ae9d248d38be164f3a9a
Karl Jose Buena <karljosebuena@gmail.com>
2020-12-31 14:34:20 +0800

    integration-testing-demo: VoterComponent - updated test for property binding and dom value/attrib validation.

--- testing-demo/src/app/integration-testing-demo/1-voter/voter.component.spec.ts
+++ testing-demo/src/app/integration-testing-demo/1-voter/voter.component.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { VoterComponent } from './voter.component';
 
 describe('VoterComponent', () => {
+  let component: VoterComponent;
+  let fixture: ComponentFixture<VoterComponent>;
 
   beforeEach(() => {
-    let component: VoterComponent;
-    let fixture: ComponentFixture<VoterComponent>;
-
     TestBed.configureTestingModule({
       declarations: [
         VoterComponent
@@ -17,6 +17,37 @@ describe('VoterComponent', () => {
     component = fixture.componentInstance;
   });
 
-  it('', () => {
+  it('should create component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should render total votes', () => {
+    component.othersVote = 20;
+    component.myVote = 1;
+    fixture.detectChanges();
+
+    const de = fixture.debugElement.query(By.css('.vote-count'));
+    const el: HTMLElement =  de.nativeElement;
+
+    expect(el.innerText).toContain('21');
+  });
+
+  it('should highlight the upVote button if upvoted', () => {
+    component.myVote = 1;
+    fixture.detectChanges();
+
+    const de = fixture.debugElement.query(By.css('.glyphicon-menu-up'));
+    const el: HTMLElement =  de.nativeElement;
+
+    expect(el.classList).toContain('highlighted');
+  });
+
+  it('should highlight the downVote button if downvoted', () => {
+    component.myVote = -1;
+    fixture.detectChanges();
+
+    const de = fixture.debugElement.query(By.css('.glyphicon-menu-down'));
+
+    expect(de.classes['highlighted']).toBeTruthy();
   });
 });
