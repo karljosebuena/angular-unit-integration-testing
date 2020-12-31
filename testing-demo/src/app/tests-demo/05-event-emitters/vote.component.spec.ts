09c2500aa37e4f38feede8bb346b748a786d6d7a
Karl Jose Buena <karljosebuena@gmail.com>
2020-12-31 11:53:59 +0800

    event-emitters: updated test for VoteComponent

--- testing-demo/src/app/tests-demo/05-event-emitters/vote.component.spec.ts
+++ testing-demo/src/app/tests-demo/05-event-emitters/vote.component.spec.ts
@@ -1,12 +1,24 @@
-import { VoteComponent } from './vote.component'; 
+import { VoteComponent } from './vote.component';
 
 describe('VoteComponent', () => {
-  var component: VoteComponent; 
+  var component: VoteComponent;
 
   beforeEach(() => {
     component = new VoteComponent();
   });
 
-  it('', () => {
+  it('shoud increment totalVotes when upvoted', () => {
+    component.upVote();
+
+    expect(component.totalVotes).toBe(1);
+  });
+
+  it('shoud raise voteChanged event when upvoted', () => {
+    let totalVotes;
+    component.voteChanged.subscribe(tv => totalVotes = tv);
+
+    component.upVote();
+
+    expect(totalVotes).toBe(1);
   });
 });
