1364d59fa3e5ebd8812c80d08f6590c8d23b1dad
Karl Jose Buena <karljosebuena@gmail.com>
2020-12-31 11:42:13 +0800

    forms: updated test for TodoFormComponent class

--- testing-demo/src/app/tests-demo/04-forms/todo-form.component.spec.ts
+++ testing-demo/src/app/tests-demo/04-forms/todo-form.component.spec.ts
@@ -1,15 +1,23 @@
-import { TodoFormComponent } from './todo-form.component'; 
+import { FormBuilder } from '@angular/forms';
+import { TodoFormComponent } from './todo-form.component';
 
 describe('TodoFormComponent', () => {
-  var component: TodoFormComponent; 
+  var component: TodoFormComponent;
 
   beforeEach(() => {
-
+    component = new TodoFormComponent(new FormBuilder);
   });
 
-  it('', () => {
+  it('should create a form with 2 form controls', () => {
+    expect(component.form.contains('name')).toBe(true);
+    expect(component.form.contains('email')).toBeTruthy()
   });
 
-  it('', () => {
+  it('should make the name control required', () => {
+    const control = component.form.controls.name;
+
+    control.setValue('');
+
+    expect(control.valid).toBeFalsy();
   });
 });
