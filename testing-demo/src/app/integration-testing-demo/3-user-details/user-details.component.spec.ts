6deca6bdaf39a85d315dbc3feb18e78d9e2f0131
Karl Jose Buena <karljosebuena@gmail.com>
2020-12-31 16:32:24 +0800

    integration-testing-demo: UserDetailsComponent - added test for Router

--- testing-demo/src/app/integration-testing-demo/3-user-details/user-details.component.spec.ts
+++ testing-demo/src/app/integration-testing-demo/3-user-details/user-details.component.spec.ts
@@ -1,28 +1,49 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserDetailsComponent } from './user-details.component';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable, empty } from 'rxjs';
 
-xdescribe('UserDetailsComponent', () => {
+class RouterStub {
+  navigate(params) {
+  }
+}
+
+class ActivatedRouteStub {
+  params: Observable<any> = empty();
+}
+
+describe('UserDetailsComponent', () => {
   let component: UserDetailsComponent;
   let fixture: ComponentFixture<UserDetailsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ UserDetailsComponent ]
+      declarations: [UserDetailsComponent],
+      providers: [
+        { provide: Router, useClass: RouterStub },
+        { provide: ActivatedRoute, useClass: ActivatedRouteStub },
+      ]
     })
-    .compileComponents();
-  }));
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(UserDetailsComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should redirect the user to the users page after saving', () => {
+    const router = TestBed.get(Router);
+    const spy = spyOn(router, 'navigate');
+
+    component.save();
+    fixture.detectChanges();
+
+    expect(spy).toHaveBeenCalledOnceWith(['users']);
   });
 });
