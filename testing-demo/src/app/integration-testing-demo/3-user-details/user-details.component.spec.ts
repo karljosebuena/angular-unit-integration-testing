8df9771c3104950c97ceeac7daec5eb5e599f89d
Karl Jose Buena <karljosebuena@gmail.com>
2020-12-31 17:10:41 +0800

    integration-testing-demo: Added test for route /not-found

--- testing-demo/src/app/integration-testing-demo/3-user-details/user-details.component.spec.ts
+++ testing-demo/src/app/integration-testing-demo/3-user-details/user-details.component.spec.ts
@@ -3,7 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserDetailsComponent } from './user-details.component';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Observable, empty } from 'rxjs';
+import { Subject } from 'rxjs';
 
 class RouterStub {
   navigate(params) {
@@ -11,26 +11,35 @@ class RouterStub {
 }
 
 class ActivatedRouteStub {
-  params: Observable<any> = empty();
+  private subject = new Subject();
+
+  push(value) {
+    this.subject.next(value);
+  }
+
+  get params() {
+    return this.subject.asObservable();
+  }
 }
 
 describe('UserDetailsComponent', () => {
   let component: UserDetailsComponent;
   let fixture: ComponentFixture<UserDetailsComponent>;
 
-  beforeEach(() => {
+  beforeEach(async() => {
     TestBed.configureTestingModule({
       declarations: [UserDetailsComponent],
       providers: [
         { provide: Router, useClass: RouterStub },
         { provide: ActivatedRoute, useClass: ActivatedRouteStub },
       ]
-    })
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(UserDetailsComponent);
     component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   it('should create', () => {
@@ -42,8 +51,17 @@ describe('UserDetailsComponent', () => {
     const spy = spyOn(router, 'navigate');
 
     component.save();
-    fixture.detectChanges();
 
     expect(spy).toHaveBeenCalledOnceWith(['users']);
+  });
+
+  it('should navigate the user to the not found page when invalid user id is passed', () => {
+    const router = TestBed.get(Router);
+    const route: ActivatedRouteStub = TestBed.get(ActivatedRoute);
+    const spy = spyOn(router, 'navigate');
+
+    route.push({id: 0});
+
+    expect(spy).toHaveBeenCalledOnceWith(['not-found']);
   });
 });
